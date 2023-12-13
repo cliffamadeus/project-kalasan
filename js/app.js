2add4f0c77a9b0aec7531ba675a66de9e417519d
Sir Cliff <dev.clfm5t12@gmail.com>
2023-12-13 14:01:48 +0800

    Added number of areas and number of trees

--- js/app.js
+++ js/app.js
@@ -1,35 +1,29 @@
 // Function to fetch external data
 async function fetchData() {
-    try {
-      const response = await fetch('./json/trees.json'); // Replace with your API endpoint
-      const data = await response.json();
-      renderTreePark(data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+  try {
+    const response = await fetch('./json/trees.json'); // Replace with your API endpoint
+    const data = await response.json();
+    renderTreePark(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
   }
-  
-  function renderTreePark(data) {
-    const treeParkContainer = document.getElementById('treeParkContainer');
-    const totalTreesContainer = document.getElementById('totalTrees');
-    let totalTreesPlanted = 0;
-  
-    data.forEach(area => {
-      const areaDiv = document.createElement('div');
-      let areaTreesHTML = area.area_trees.map(tree => {
-        totalTreesPlanted++;
-      }).join('');
-  
-      areaDiv.innerHTML = `
-        <p class="total-trees-planted">${totalTreesPlanted}</p>
-        <hr>
-        <p class="total-trees-planted-date">Total trees planted as of ${area.area_created_date}</p>
-      `;
-  
-      treeParkContainer.appendChild(areaDiv);
-    });
-  }
-  
-  // Fetch and render the tree park data
-  fetchData();
-  
+}
+
+function renderTreePark(data) {
+  const totalTreesContainer = document.getElementById('totalTrees');
+  let totalAreas = data.length;
+  let totalTrees = 0;
+
+  data.forEach(area => {
+    totalTrees += area.area_trees.length;
+  });
+
+  // Display the total count of areas and trees
+  totalTreesContainer.innerHTML = `
+    <p>Total areas: ${totalAreas}</p>
+    <p>Total trees: ${totalTrees}</p>
+  `;
+}
+
+// Fetch and render the tree park data
+fetchData();
