15d0f567a95e165d578562a1e3af2d55c43c4e24
Cliffy <dev.clfm5t12@gmail.com>
2023-12-13 18:57:30 +0800

    Added desc sorting on recent records

--- js/map-script.js
+++ js/map-script.js
@@ -82,34 +82,44 @@ fetch('json/trees.json')
       });
     });
     map.addLayer(markers);
-
     
-    //Preload
-    
+    //Recent Records
     const recentRecordsContainer = document.getElementById('recentRecordsContainer');
     recentRecordsContainer.innerHTML = generateHTMLContent(data);
 
-      function generateHTMLContent(data) {
-        return data.map(area => {
-          const areaHTML = `
-            <div >
-                ${area.area_trees.slice(0, 3).map(tree => 
-                  `
-                  <div class="map-preload-item">
-                  <span style="display: inline-block;">
-                    <a href="${tree.tree_id.trim()}" class="map-recent-records-header">${tree.tree_id.trim()}</a>
-                    ${tree.tree_planted_by} has recently planted a tree
-                    <br>Area: ${area.area_name} 
-                  </span>
-                  </div>
-                  <hr style="border-top: 1px solid #ccc; margin: 5px 0;">
-                `)
-                .join('')}
+    function generateHTMLContent(data) {
+      // Flatten the array of trees across all areas
+      const allTrees = data.flatMap(area => area.area_trees);
+    
+      // Sort the flattened array in descending order based on tree_id
+      const sortedTrees = allTrees
+        .sort((a, b) => parseInt(b.tree_id, 10) - parseInt(a.tree_id, 10))
+        .slice(0, 5); // Take the top 5 trees
+    
+      // Find the default area name (using the first area in the data)
+      const defaultAreaName = data.length > 0 ? data[0].area_name : 'Unknown Area';
+    
+      // Generate HTML content
+      const htmlContent = `
+        <div>
+          ${sortedTrees.map(tree => 
+            `
+            <div class="map-preload-item" onclick="flyToLocation(${tree.tree_lat}, ${tree.tree_lng})">
+              <span style="display: inline-block;">
+                <a href="${tree.tree_id.trim()}" class="map-recent-records-header">${tree.tree_id.trim()}</a>
+                ${tree.tree_planted_by} has recently planted a tree
+                <br>Area: ${tree.area_name || defaultAreaName} 
+              </span>
             </div>
-          `;
-          return areaHTML;
-        }).join('');
-      }
+            <hr style="border-top: 1px solid #ccc; margin: 5px 0;">
+            `)
+            .join('')}
+        </div>
+      `;
+    
+      return htmlContent;
+    }
+
     //Search
       searchData = data;
       const searchInputBar = document.getElementById('searchInputBar');
@@ -155,8 +165,6 @@ fetch('json/trees.json')
                   areaTreeCountMap[areaName] = (areaTreeCountMap[areaName] || 0) + 1;
               });
       
-              
-
               Object.entries(areaTreeCountMap).forEach(([areaName, treeCount]) => {
                   const searchCardItem = document.createElement('li');
                   searchCardItem.innerHTML = `
